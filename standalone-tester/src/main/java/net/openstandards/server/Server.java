a693119cfd7df1cded7ad074936b85bca7939d38
erik777 <e_github-erik777@openstandards.net>
2022-02-09 12:59:42 -0500

    Changed standalone tester to be a clustered vertx.

--- standalone-tester/src/main/java/net/openstandards/server/Server.java
+++ standalone-tester/src/main/java/net/openstandards/server/Server.java
@@ -12,22 +12,34 @@ public class Server {
   private static final Logger log = LoggerFactory.getLogger(Server.class);
   
   public static void main(String[] args) {
+    new Server().start();
+  }
+  
+  public void start() {
     log.debug("new VertxOptions().getWorkerPoolSize: " + new VertxOptions().getWorkerPoolSize());
-    Vertx vertx = Vertx.vertx();
-    // Worker verticles are for blocking code.  Creating a thread pool of 40 for them (default 20)
-//    Vertx vertx = Vertx.vertx(new VertxOptions().setWorkerPoolSize(40));
+    Vertx.clusteredVertx(new VertxOptions(), v -> {
+      if (v.succeeded()) {
+        Vertx vertx = v.result();
+        launch(vertx);
+      }
+    });
+  }
+  
+  public void launch(Vertx vertx) {
+      // Worker verticles are for blocking code.  Creating a thread pool of 40 for them (default 20)
+  //  Vertx vertx = Vertx.vertx(new VertxOptions().setWorkerPoolSize(40));
     
     // Designate this a worker (blocking) verticale 
     vertx.deployVerticle("net.openstandards.vertx.SeContainerVerticle"
         , 
         new DeploymentOptions().setInstances(2)
-//    new DeploymentOptions().setInstances(2).setWorker(true)
+  //  new DeploymentOptions().setInstances(2).setWorker(true)
     );
-
+  
     // an event loop (non-blocking) verticle
-//    vertx.deployVerticle("net.openstandards.vertx.HelloVerticle"
-//        , new DeploymentOptions().setInstances(2).setWorker(true)
-//        );
+  //  vertx.deployVerticle("net.openstandards.vertx.HelloVerticle"
+  //      , new DeploymentOptions().setInstances(2).setWorker(true)
+  //      );
     vertx.deployVerticle(new HelloVerticle("H1"), new DeploymentOptions().setWorker(true));
     vertx.deployVerticle(new HelloVerticle("H2"), new DeploymentOptions().setWorker(true));
   }
